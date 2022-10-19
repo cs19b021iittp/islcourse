2f76cf61c09df8bb411c10ecb267cd10dd91874e
CS19B021 <70951042+cs19b021iittp@users.noreply.github.com>
2022-10-19 11:37:16 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -76,35 +76,34 @@ def get_model(train_data_loader=None, n_epochs=10):
 
 training_data, test_data = load_data();
 train_dataloader, test_dataloader = create_dataloaders(training_data, test_data, batch_size=64)
-  print ('Returning model... (cs19b021: xx)')
+  
   
   return model
 
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
-  model = None
+  model = get_model(train_data_loader=train_dataloader, n_epochs=10);
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  # In addition,
-  # Refer to config dict, where learning rate is given, 
-  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-  # You need to create 2d convoution layers as per specification above in each element
-  # You need to add a proper fully connected layer as the last layer
-  
-  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-  # HINT: Flatten function can also be used if required
-  return model
-  
-  
-  print ('Returning model... (rollnumber: xx)')
-  
+  # To train a model, we need a loss function and an optimizer.
+  optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+  size = len(train_dataloader.dataset)
+  model.train()
+  for batch, (X, y) in enumerate(train_dataloader):
+      X, y = X.to(device), y.to(device)
+
+      # Compute prediction error
+      pred = model(X)
+      loss = loss_fn(pred, y)
+
+      # Backpropagation
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+      if batch % 100 == 0:
+          loss, current = loss.item(), batch * len(X)
+          print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
   return model
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
