549fe8480376d7dbbc0bee77e9ab21e85ebb97a7
CS19B021 <70951042+cs19b021iittp@users.noreply.github.com>
2022-11-21 12:16:09 +0530

    part 3 partially done

--- hubconf.py
+++ hubconf.py
@@ -3,19 +3,15 @@ import scipy
 import seaborn
 import numpy as np
 import pandas as pd
+import torch
+import torchvision
+import torch.nn as tn
+import torchvision.transforms as tt
+import torch.utils as utils
 
 %matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn
-
-
-# You can import whatever standard packages are required
-
-# full sklearn, full pytorch, pandas, matplotlib, numpy are all available
-# Ideally you do not need to pip install any other packages!
-# Avoid pip install requirement on the evaluation program side, if you use above packages and sub-packages of them, then that is fine!
-
-###### PART 1 ######
 
 from sklearn.datasets import make_blobs
 def get_data_blobs(n_points=100):
@@ -35,15 +31,15 @@ def get_data_circles(n_points=100):
   # write your code ...
   return X,y
 
-
-from sklearn.datasets import load_digits
+from sklearn.datasets import fetch_openml
 def get_data_mnist():
   pass
   # write your code here
   # Refer to sklearn data sets
-  digits = load_digits()
-  X=digits.data
-  y=digits.target
+
+  mnist = fetch_openml('MNIST original')
+  X=mnist.data
+  y=mnist.target
   # write your code ...
   return X,y
 
@@ -78,8 +74,6 @@ def compare_clusterings(ypred_1=None,ypred_2=None):
   v=metrics.v_measure_score(ypred_1, ypred_2)
   return h,c,v
 
-###### PART 2 ######
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
@@ -108,32 +102,31 @@ def get_metrics(model=None,X=None,y=None):
   auc = roc_auc_score(y, model.predict_proba(X), multi_class='ovr' )
   return acc, prec, rec, f1, auc
 
-from sklearn.model_selection import train_test_split
-X, y = get_data_mnist()
-Xtrain,Xtest,ytrain,ytest = train_test_split(X,y,test_size=0.3)
-
-lr_model = build_lr_model(Xtrain, ytrain)
-rf_model = build_rf_model(Xtrain, ytrain)
-
-print(get_metrics(lr_model, Xtest, ytest))
-print(get_metrics(rf_model, Xtest, ytest))
-
+from sklearn.model_selection import GridSearchCV
 def get_paramgrid_lr():
   # you need to return parameter grid dictionary for use in grid search cv
   # penalty: l1 or l2
-  lr_param_grid = None
-  # refer to sklearn documentation on grid search and logistic regression
-  # write your code here...
-  return lr_param_grid
+
+   lr_param_grid = {
+      "max_iter": [100, 200, 500],
+      "penalty": ["l1","l2"],
+      "solver" : ["liblinear"]
+   }
+
+   return lr_param_grid
 
 def get_paramgrid_rf():
   # you need to return parameter grid dictionary for use in grid search cv
   # n_estimators: 1, 10, 100
   # criterion: gini, entropy
   # maximum depth: 1, 10, None  
-  rf_param_grid = None
-  # refer to sklearn documentation on grid search and random forest classifier
-  # write your code here...
+  rf_param_grid = { 
+    'n_estimators' : [1, 10, 100],
+    'max_depth' : [1,10,None],
+    'criterion' :['gini', 'entropy'],
+
+  }
+
   return rf_param_grid
 
 def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
@@ -144,41 +137,36 @@ def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None,
   
   # metrics = [] the evaluation program can change what metrics to choose
   
-  grid_search_cv = None
-  # create a grid search cv object
-  # fit the object on X and y input above
-  # write your code here...
-  
-  # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
-  
-  # refer to cv_results_ dictonary
-  # return top 1 score for each of the metrics given, in the order given in metrics=... list
-  
+  print(model.get_params().keys())
   top1_scores = []
-  
+  for scoring in metrics:
+    grid_search_cv = GridSearchCV(model,param_grid, cv=cv, scoring=scoring)
+    grid_search_cv.fit(X,y)
+    top1_scores.append(grid_search_cv.best_score_)
+
   return top1_scores
 
-###### PART 3 ######
-
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, TensorDataset
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class MyNN(nn.Module):
   def __init__(self,inp_dim=64,hid_dim=13,num_classes=10):
     super(MyNN,self).__init__()
     
-    self.fc_encoder = nn.Linear(in_features=inp_dim,out_features=hid_dim)
-    self.fc_decoder = nn.Linear(in_features=hid_dim,out_features=inp_dim) # write your code hid_dim to inp_dim mapper
-    self.fc_classifier = nn.Linear(in_features=hid_dim,out_features=num_classes) # write your code to map hid_dim to num_classes
+    self.fc_encoder = nn.Sequential(nn.Linear(in_features=inp_dim,out_features=hid_dim),)
+    self.fc_decoder = nn.Sequential(nn.Linear(in_features=hid_dim,out_features=inp_dim),) # write your code hid_dim to inp_dim mapper
+    self.fc_classifier = nn.Sequential(nn.Linear(in_features=hid_dim,out_features=num_classes),) # write your code to map hid_dim to num_classes
     
     self.relu = nn.ReLU() #write your code - relu object
     self.softmax =nn.softmax() #write your code - softmax object
+
     
   def forward(self,x):
-    x = torch.nn.functional.normalize(x, p=2.0, dim = 0) # write your code - flatten x
+    x = nn.Flatten(x) # write your code - flatten x
     x_enc = self.fc_encoder(x)
     x_enc = self.relu(x_enc)
     
@@ -194,7 +182,8 @@ class MyNN(nn.Module):
     
     # class prediction loss
     # yground needs to be one hot encoded - write your code
-    lc1 = None # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
+    x=(y_pred-yground)**2
+    lc1 = torch.mean(x) # write your code for cross entropy between yground and y_pred, advised to use torch.mean()
     
     # auto encoding loss
     lc2 = torch.mean((x - xencdec)**2)
@@ -209,9 +198,27 @@ def get_mynn(inp_dim=64,hid_dim=13,num_classes=10):
   return mynn
 
 def get_mnist_tensor():
-  # download sklearn mnist
+  # download sklearn mnist\
+  training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    )
+
+# Download test data from open datasets.
+  test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+  )
   # convert to tensor
-  X, y = None, None
+  trainset = datasets.FashionMNIST(root='.', train=True, transform=ToTensor(), download=True)
+  testset = datasets.FashionMNIST(root='.', transform=ToTensor(), download=True)
+  train_loader = DataLoader(trainset, batch_size=32, shuffle=True)
+  test_loader = DataLoader(testset, batch_size=32)
+  X, y = trainset,testset
   # write your code
   return X,y
 
